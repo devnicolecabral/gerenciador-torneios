a81511292c2165fb58fea287c8e0841e512c114a
Nicole Cabral <nscnick22@gmail.com>
2026-02-25 11:01:06 -0300

    Alterações no main e adição do arquivo de processamento de dados

--- cm_api.py
+++ cm_api.py
@@ -35,9 +35,53 @@ def pegar_cracha_cm():
     except Exception as e:
         print(f"Aconteceu um erro no código: {e}") 
         return None
+    
 
-if __name__ == "__main__":
+def buscar_jogadores(torneio_id):
+    # Pega o crachá temporário 
     token = pegar_cracha_cm()
-    if token: 
-        print(f"Seu token de acesso começa com: {token[:15]}...")
+    
+    if not token:
+        print("Sem crachá, não podemos prosseguir.")
+        return None
         
+    print(f"Buscando inscritos do torneio: {torneio_id}...")
+    
+    # Endereço da API para buscar os participantes 
+    url_participantes = f"https://publicapi.challengermode.com/mk1/v1/tournaments/{torneio_id}/participants"
+    
+    # Colocamos o nosso crachá no header do pedido
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Accept": "application/json"
+    }
+    
+    try:
+        # Fazemos o pedido de leitura (GET)
+        resposta = requests.get(url_participantes, headers=headers)
+        
+        if resposta.status_code == 200:
+            dados = resposta.json()
+            print("Lista de jogadores lida com sucesso!")
+            return dados
+        else:
+            print(f"Erro ao ler participantes. Código: {resposta.status_code}")
+            print(f"Detalhes: {resposta.text}")
+            return None
+            
+    except Exception as e:
+        print(f"Aconteceu um erro no código: {e}")
+        return None
+
+# --- Área de Teste ---
+if __name__ == "__main__":
+    # Colar o ID do seu torneio de teste aqui dentro das aspas quando tiver
+    ID_TESTE = "ID_DO_TORNEIO_AQUI" 
+    
+    dados_torneio = buscar_jogadores(ID_TESTE)
+    
+    if dados_torneio:
+        print("-" * 30)
+        print("A API devolveu isso aqui (amostra):")
+        # Imprime só os primeiros 500 caracteres para não travar a tela
+        print(str(dados_torneio)[:500] + " ... [tem mais coisas]")
---
+++ processador.py
@@ -0,0 +1,33 @@
+import pandas as pd 
+
+# Simulação do que a API da CM vai devolver 
+
+dados_simulados = {
+    "participants": [
+        {"username": "TLV", "status": "Played", "score": 20},
+        {"username": "FakerBR", "status": "NoShow", "score": 0}, # W.O.
+        {"username": "Megalodon", "status": "Played", "score": 15},
+        {"username": "Soneca", "status": "NoShow", "score": 0} # Outro W.O.
+    ]
+}
+
+def processar_torneio(dados):
+    print("Iniciando o processamento dos dados...")
+
+    df = pd.DataFrame(dados["participants"])
+
+    print("\n Tabela original (com todos os inscritos): ")
+    print(df)
+
+    df_aprovados = df[df["Status"] != "NoShow"]
+
+    print("\n Taela limpa (Pronta pra riot - Sem W.O):")
+    print(df_aprovados)
+
+    return df_aprovados
+
+
+# --- Área de Teste ---
+if __name__ == "__main__":
+    processar_torneio(dados_simulados)
+
